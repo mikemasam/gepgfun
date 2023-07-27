8944b6890820ecd7e2dca2d61da2755fbfabf2e0
sudo <meshackmasam@gmail.com>
2023-07-27 20:27:47 +0300

    argument passing improvements

--- index.ts
+++ index.ts
@@ -26,17 +26,18 @@ if (action == "serve") {
 
   makePayment(app$argv, params[0], params[1], params[2], params[3]);
 } else if (action == "control_number") {
-  if (!param) {
+  let _param = param + "";
+  if (!_param) {
     segfalt(-1, "Invalid bill information arg - bill");
   }
-  const params: string[] = param.split(":");
+  const params: string[] = _param.split(":");
   if (params.length != 1) {
     segfalt(-1, "Invalid bill information arg - bill");
   }
   makeControlNumber(app$argv, params[0]);
 } else {
   console.log(`
-Command required:\n
+Commands:\n
 - server - start gepg server\n
 - payment bill:control_number:amount:currency - make payment\n
 - control_number bill - generate control number\n
--- src/utils.ts
+++ src/utils.ts
@@ -5,9 +5,17 @@ export async function buildXml(obj: any) {
   return b.buildObject(obj);
 }
 export async function parseXml(str: string) {
-  return xml2js.parseStringPromise(str, {
-    explicitArray: false,
-  });
+  const [out, err] = await xml2js
+    .parseStringPromise(str, {
+      explicitArray: false,
+    })
+    .then((o) => [o, ""])
+    .catch((err) => [false, err]);
+  if (err) {
+    console.log(str);
+    throw "Invalid xml format";
+  }
+  return out;
 }
 export const segfalt = (code: number, message: string) => {
   if (code == 0) {
